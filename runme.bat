ca648c8c4170c6bb91a769a8bf333b0134fd7237
SERENO Mickael <serenomickael@gmail.com>
2017-12-03 19:51:58 +0100

    script bat to run the project

---
+++ runme.bat
@@ -0,0 +1,7 @@
+start Binaries\Debug\RayTracer.exe scene01.yaml imgCubeCylinder.png
+start Binaries\Debug\RayTracer.exe scene02.yaml imgCubeCylinder2.png
+start Binaries\Debug\RayTracer.exe scene03.yaml imgCubeCylinder3.png
+start Binaries\Debug\RayTracer.exe sceneProf01.yaml imgSpheres.png
+start Binaries\Debug\RayTracer.exe sceneProf02.yaml imgSpheres2.png
+start Binaries\Debug\RayTracer.exe sceneProf03.yaml imgSpheres3.png
+pause
